b568879bb3ff10f6d64ec1b99f6a0da0090fe2ce
Joshua Duross <durossfam@gmail.com>
2023-07-18 18:26:20 -0700

    Started on my screens.h file, which will include the "knockoff" shuffleboard.
    I am going to try and make this this clone be easily implementable by others incase others want a PROS auton selector / shuffleboard.

---
+++ include/screens.h
@@ -0,0 +1,26 @@
+#ifndef SCREENS_H
+#define SCREENS_H
+
+#include "main.h"
+
+typedef void (*screenFunction)();
+
+enum screenEnum {
+    MENU,
+    SHUFFLEBOARD
+};
+
+struct screenStruct {
+    screenEnum screenType;
+    screenFunction screenDrawFunction;
+};
+
+class Display {
+    public:
+        Display(std::vector<screenStruct> screens) {
+            //Sorts the screen types, making sure that they are in order before we attempt to do anything with them. In my implementation, I believe I am going to have to make sure that "MENU" is always at zero. I'm not entirely sure what I'm going to be doing, though
+            sort(screens.begin(), screens.end(), [](const screenStruct& lhs, const screenStruct& rhs) {return lhs.screenType < rhs.screenType;});
+        }
+};
+
+#endif
