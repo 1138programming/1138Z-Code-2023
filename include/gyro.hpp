b5acee9d28bdb02696c8a1655ac041641d70442a
Goomisica <60203441+Goomisica@users.noreply.github.com>
2023-08-20 22:41:45 -0700

    added gyro code (probably broken) + need to fix main

---
+++ include/gyro.hpp
@@ -0,0 +1,17 @@
+#ifndef GYRO_HPP
+#define GYRO_HPP
+
+#include "vex.h"
+
+class Gyro {
+    vex::inertial::gyro gyro;
+    public:
+        Gyro(int gyroPort) {
+            this->gyro = pros::inertial::gyro(gyroPort);
+        }
+        void reset() {
+            this->gyro.reset();
+        }
+};
+
+#endif
