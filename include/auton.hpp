063a4a72f1cebcb5cc13d23c5e0533365fe59d56
Goomisica <60203441+Goomisica@users.noreply.github.com>
2023-08-27 23:13:04 -0700

    Changes made during last meeting

--- include/auton.hpp
+++ include/auton.hpp
@@ -9,20 +9,28 @@ class Autons {
     Base* base;
     public:
         void driveForwardForSpecifiedTime(float seconds) {
-        float initialTime = vex::timer::systemHighResolution()/1000;
+            float initialTime = vex::timer::systemHighResolution()/1000;
 
-        // while(pros::millis()/1000 - initialTime < seconds/3) {
-        //     this->base->moveLeftMotors(100);
-        //     this->base->moveRightMotors(-100);
-        // }
+            // while(pros::millis()/1000 - initialTime < seconds/3) {
+            //     this->base->moveLeftMotors(100);
+            //     this->base->moveRightMotors(-100);
+            // }
 
-        while(vex::timer::systemHighResolution()/1000 - initialTime < seconds) {
-            //wait
-            this->base->driveBothSides(kDriveForwardAutonSpeed);
-            vex::wait(10,vex::msec);
+            while(vex::timer::systemHighResolution()/1000 - initialTime < seconds) {
+                this->base->driveBothSides(kDriveForwardAutonSpeed);
+                //wait, so that the scheduler has some time to do stuff
+                vex::wait(10,vex::msec);
+            }
+            this->base->driveBothSides(0);
         }
-        this->base->driveBothSides(0);
-    }
+        void driveForwardForSpecifiedTimeAndPercent(float seconds, float percent) {
+            float initialTime = vex::timer::systemHighResolution()/1000;
+            while(vex::timer::systemHighResolution()/1000 - initialTime < seconds) {
+                this->base->driveBothSides(100 * percent);
+                vex::wait(10,vex::msec);
+            }
+            this->base->driveBothSides(0);
+        }
 };
 
 #endif
